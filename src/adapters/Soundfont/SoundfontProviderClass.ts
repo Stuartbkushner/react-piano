5777d241e4bffb7b112cfdc8fbed7ceb9abc47ab
Stuart Kushner <stuartbkushner@gmail.com>
2020-11-30 09:36:05 -0500

    rendered props by class

--- src/adapters/Soundfont/SoundfontProviderClass.ts
+++ src/adapters/Soundfont/SoundfontProviderClass.ts
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react"
+import { Component, ReactElement } from "react"
 import Soundfont, { InstrumentName, Player } from "soundfont-player"
 import { MidiValue } from "../../domain/note"
 import { Optional } from "../../domain/types"
@@ -7,70 +7,100 @@ import {
   DEFAULT_INSTRUMENT
 } from "../../domain/sound"
 
-interface Settings {
-  AudioContext: AudioContextType
-}
-
-interface Adapted {
+interface ProvidedProps {
   loading: boolean
-  current: Optional<InstrumentName>
-
-  load(instrument?: InstrumentName): Promise<void>
   play(note: MidiValue): Promise<void>
   stop(note: MidiValue): Promise<void>
 }
 
-export function useSoundfont({ AudioContext }: Settings): Adapted {
-  let activeNodes: AudioNodesRegistry = {}
-  const [current, setCurrent] = useState<Optional<InstrumentName>>(
-    null
-  )
-  const [loading, setLoading] = useState<boolean>(false)
-  const [player, setPlayer] = useState<Optional<Player>>(null)
-  const audio = useRef(new AudioContext())
-
-  async function resume() {
-    return audio.current.state === "suspended"
-      ? await audio.current.resume()
-      : Promise.resolve()
-  }
-
-  async function load(
-    instrument: InstrumentName = DEFAULT_INSTRUMENT
-  ) {
-    setLoading(true)
-    const player = await Soundfont.instrument(
-      audio.current,
-      instrument
-    )
-
-    setLoading(false)
-    setCurrent(instrument)
-    setPlayer(player)
-  }
-
-  async function play(note: MidiValue) {
-    await resume()
-    if (!player) return
-
-    const node = player.play(note.toString())
-    activeNodes = { ...activeNodes, [note]: node }
-  }
-
-  async function stop(note: MidiValue) {
-    await resume()
-    if (!activeNodes[note]) return
-
-    activeNodes[note]!.stop()
-    activeNodes = { ...activeNodes, [note]: null }
-  }
-
-  return {
-    loading,
-    current,
-
-    load,
-    play,
-    stop
-  }
+interface ProviderProps {
+  instrument: InstrumentName
+  AudioContext: AudioContextType
+  render(props: ProvidedProps): ReactElement
 }
+
+interface ProviderState {
+  loading: boolean
+  current: Optional<InstrumentName>
+}
+
+export class SoundfontProvider extends Component<
+  ProviderProps,
+  ProviderState
+> {
+    public static defaultProps = {
+      instrument: DEFAULT_INSTRUMENT
+    }
+
+    private audio: AudioContext
+    private player: Optional<Player> = null
+    private activeNodes: AudioNodesRegistry = {}
+
+    public state: ProviderState = {
+      loading: false,
+      current: null
+    }
+
+    private resume = async () => {
+      return this.audio.state === "suspended"
+        ? await this.audio.resume
+        : Promise.resolve()
+    }
+
+    private load = async (instrument: InstrumentName) => {
+      this.setState({ loading: true })
+      this.player = await Soundfont.instrument(this.audio, instrument)
+
+      this.setState({ loading: false, current: instrument })
+    }
+
+    constructor(props: ProviderProps) {
+      super(props)
+
+      const { AudioContext } = this.props
+      this.audio = new AudioContext()
+    }
+
+    public componentDidMount() {
+      const { instrument } = this.props
+      this.load(instrument)
+    }
+
+    public shouldComponentUpdate({ instrument }: ProviderProps) {
+      return this.state.current !== instrument
+    }
+
+    public componentDidUpdate({
+      instrument: prevInstrument
+    }: ProviderProps) {
+      const { instrument } = this.props
+      if (instrument && instrument !== prevInstrument)
+        this.load(instrument)
+    }
+
+    public play = async (note: MidiValue) => {
+      await this.resume()
+      if (!this.player) return
+
+      const node = this.player.play(note.toString())
+      this.activeNodes = { ...this.activeNodes, [note]: node }
+    }
+
+    public stop = async (note: MidiValue) => {
+      await this.resume()
+      if (!this.activeNodes[note]) return
+      this.activeNodes[note]!.stop()
+      this.activeNodes = { ...this.activeNodes, [note]: null}
+    }
+
+    public render() {
+      const { render } = this.props
+      const { loading } = this.state
+
+      return render({
+        loading,
+        play: this.play,
+        stop: this.stop
+      })
+    }
+  }
