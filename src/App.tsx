a9a08fe98fd58b77ec18af267c5bca79123a4e97
Stuart Kushner <stuartbkushner@gmail.com>
2020-11-30 10:14:09 -0500

    Removed logos and replaced them with a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { Footer } from "./components/Footer"
-import { Logo } from "./components/Logo"
+import { Header } from "./components/Header"
 import { Main } from "./components/Main"
 import "./App.css"
 
 export const App = () => {
   return (
     <div className="app">
-      <Logo />
+      <Header />
       <main className="app-content">
         <Main />
       </main>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from "react"
+import "./style.css"
+
+export const Header: FunctionComponent = () => {
+  return (
+    <h1 className="header">
+      Music For Everyone!
+    </h1>
+  )
+}
